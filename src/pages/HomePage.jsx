fb5ca7cce1982da364d949fa7551a9b83b117a3a
solifIore <shunsuke630@outlook.com>
2025-09-02 23:17:52 +0900

    create test page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/HomePage.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="content">
+        <header className="title">HomePage</header>
+        <p>準備中</p>
+        <img src="src/assets/figure_ojigi.png" alt="Ojigi" />
+      </div>
+    </>
+  );
+}
